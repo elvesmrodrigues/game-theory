57d6f42e15955eafdcd32b51b04367c87b6e70b5
EstefanoB <estefanobvflu10@gmail.com>
2022-06-25 18:31:52 -0300

    Add function to get class name from a .py.
    Implemented function "get_player_class_name" which, given a .py file,
    returns the name of the child class defined inside of it.

---
+++ src/player/dynamic_imports.py
@@ -0,0 +1,34 @@
+from typing import List
+from pathlib import Path
+
+import re
+
+
+def get_player_class_name(path_to_file: Path):
+
+    """
+        Given a .py file, returns its class name.
+
+        It is assumed that the class is inheriting from Player, 
+        i.e. the line with its definition should be like 
+        "class <CLASS_NAME><some_spaces_maybe>(Player)<rest>".
+    """
+
+    regex_pattern = re.compile(r"class (?P<class_name>.+?)\s*\(")
+
+    with path_to_file.open("r") as file:
+
+        for line in file:
+            regex_match = re.match(regex_pattern, line)
+
+            if regex_match is not None:
+                return regex_match.group("class_name")
+
+        raise NameError("No class has been found.")
+
+
+if __name__ == "__main__":
+
+    test_path = Path("./test_class.py")
+
+    print(f"Function: {get_player_class_name(test_path)}\nExpected: TestClass")
---
+++ src/player/test_class.py
@@ -0,0 +1,5 @@
+from player import Player
+
+
+class TestClass (Player):
+    pass
