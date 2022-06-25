aa6994b2adeea143698fc3f2f6de4fc92cb74f40
Elves M. Rodrigues <elvesmateusrodrigues@outlook.com>
2022-06-25 16:36:43 -0300

    Player class if example

--- src/game/game.py
+++ src/game/game.py
@@ -8,19 +8,16 @@ class Game:
     def __init__(self, game_type: str, player_row_payoff_matrix: List[List[Union[float, int]]], player_col_payoff_matrix: List[List[Union[float, int]]]) -> None:
         self.type = game_type
 
-        self.player_row_payoff_matrix = player_row_payoff_matrix
-        self.player_col_payoff_matrix = player_col_payoff_matrix
+        self.payoff_matrix = dict()
+        for row in range(len(player_row_payoff_matrix)):
+            self.payoff_matrix[f'R{row}'] = dict()
+            for col in range(len(player_row_payoff_matrix[0])):
+                self.payoff_matrix[f'C{col}'] = player_row_payoff_matrix[row][col]
 
-        num_row_player_action = len(player_row_payoff_matrix)
-        num_col_player_action = len(player_row_payoff_matrix[0])
-
-        self.player_row_available_actions = list(range(num_row_player_action))
-        self.player_col_available_actions = list(range(num_col_player_action))
-
-    def get_payoff(self, player: Literal['row', 'col'], player_row_action: int, player_col_action: int) -> Union[float, int]:
-        if player == 'row':
-            return self.player_row_payoff_matrix[player_row_action][player_col_action]
-        return self.player_col_payoff_matrix[player_row_action][player_col_action]
+                if f'C{col}' not in self.payoff_matrix:
+                    self.payoff_matrix[f'C{col}'] = dict()
+                
+                self.payoff_matrix[f'C{col}'][f'R{row}'] = player_col_payoff_matrix[row][col]
 
     def play(self, player_row: Player, player_col: Player) -> Tuple[Union[float, int], Union[float, int]]:
         player_row_action = player_row.get_action(self.type, 
--- src/player/player.py
+++ src/player/player.py
@@ -1,19 +1,106 @@
-from typing import List, Union
-
+from typing import Dict, List, Union
+from random import randint
 
 class Player:
     def __init__(self) -> None:
-        pass
+        '''
+        memory format: 
+        {
+            'game_type': {
+                'adversary_id': [
+                    'action_taken': str
+                    'available_action': []
+                    'adversary_action_taken': str,
+                    'adversary_available_action': [] 
+                ]
+            }
+        }
+        '''
+        self.memory = dict()
     
     def get_action(self, game_type: str, 
                         adversary_id: str,
-                        available_action: List[int], 
-                        adversary_available_action: List[int], 
-                        payoff_matrix: List[List[Union[float, int]]], 
-                        adversary_payoff_matrix: List[List[Union[float, int]]]) -> int:
+                        available_action: List[str], 
+                        adversary_available_action: List[str], 
+                        payoff_matrix: Dict[str, Dict[str, Union[float, int]]]) -> int:
 
         '''Retorna a ação do jogador quando ele recebe a matrix de payoff, suas ações disponíveis, etc
         '''
         raise NotImplementedError('You must implement your own method')
 
-    def update_knowledge(self, game_type: str, payoff: Union[float,int], action_taken: int, adversary_id: str, action_taken_by_adversary: int)
+    def update_knowledge(self, game_type: str, 
+                                adversary_id: str, 
+                                payoff_matrix: Dict[str, Dict[str, Union[float, int]]], 
+                                action_taken: str, 
+                                available_actions: List[str] , 
+                                action_taken_by_adversary: str, 
+                                adversary_available_action: List[str]) -> None:
+
+        if game_type not in self.memory:
+            self.memory[game_type] = dict()
+
+        if adversary_id not in self.memory[game_type]:
+            self.memory[game_type][adversary_id] = list()
+
+        self.memory[game_type][adversary_id].append({
+            'action_taken': action_taken,
+            'available_actions': available_actions,
+            'adversary_action_taken': action_taken_by_adversary,
+            'adversary_available_action': adversary_available_action,
+            'payoff_matrix': payoff_matrix
+        })
+
+# Classe apenas de exemplo
+class RandomPlayer(Player):
+    def get_action(self, game_type: str, 
+                        adversary_id: str, 
+                        available_action: List[str], 
+                        adversary_available_action: List[str], 
+                        payoff_matrix: Dict[str, Dict[str, Union[float, int]]]) -> int:
+
+        return available_action[randint(0, len(available_action) - 1)]
+
+if __name__ == '__main__':
+    '''
+    Matrix de payoff abaixo:
+
+            C0       C1
+        -----------------
+    R0  | (1,1)  | (10,0)|
+        -----------------
+    R1  | (0,10) | (5,5) |
+        ------------------
+    
+    Deverá ser representada como:
+
+    '''
+    payoff_matrix = {
+        'R0': {
+            'C0': 1,
+            'C1': 10 
+        },
+        'R1': {
+            'C0': 0,
+            'C1': 5
+        },
+        'C0': {
+            'R0': 1,
+            'R1': 10
+        },
+        'C1': {
+            'R0': 0,
+            'R1': 5
+        }
+    }
+
+    random_player_1 = RandomPlayer()
+    random_player_2 = RandomPlayer()
+
+    player_row_available_action = ['R0', 'R1']
+    player_col_available_action = ['C0', 'C1']
+
+    action_player_1 = random_player_1.get_action('test', None, player_row_available_action, player_col_available_action, payoff_matrix)
+    action_player_2 = random_player_2.get_action('test', None, player_col_available_action, player_row_available_action, payoff_matrix)
+
+    print(f'Payoff of player row: {payoff_matrix[action_player_1][action_player_2]}')
+    print(f'Payoff of player col: {payoff_matrix[action_player_2][action_player_1]}')
