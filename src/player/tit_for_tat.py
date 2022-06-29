35502156132da4b5bea46b1cff63f3f44d133a0f
EstefanoB <estefanobvflu10@gmail.com>
2022-06-29 17:17:52 -0300

    Add tit for tat strategy

---
+++ src/player/tit_for_tat.py
@@ -0,0 +1,34 @@
+from enum import Enum
+from typing import Optional
+from src.player.player import Player, MatchHistory, PayoffMatrix
+
+
+class Action(Enum):
+    COOPERATE = 0
+    DEFECT = 1
+
+
+class TitForTat(Player):
+
+
+    def __init__(self) -> None:
+        self.name = "Tit for Tat"
+        self.match_history = dict()
+
+
+    def get_action(
+        self, 
+        game_type: str, 
+        payoff_matrix: PayoffMatrix,
+        adversary_id: str,
+        match_history: Optional[MatchHistory],
+        row_or_col: str
+    ) -> int:
+
+        if match_history is None:
+            return Action.COOPERATE
+        
+        _, adversary_previous_action = match_history[row_or_col][-1]
+        return adversary_previous_action
+
+        
