9f16713397e0fc0bcd2abf109e7e4f915794b1ce
EstefanoB <estefanobvflu10@gmail.com>
2022-06-29 11:45:58 -0300

    Fix tests for Player

--- tests/player/test_player_abc.py
+++ tests/player/test_player_abc.py
@@ -1,7 +1,7 @@
 import pytest
-from src.player.player import Player
+from src.player.player import Player, PayoffMatrixType, MatchHistoryType
 
-from typing import Tuple, Dict, Union
+from typing import Tuple, Dict, Union, Optional
 
 PlayerFuncParam = Union[str, Tuple[str], Dict[str, Dict[str, Union[float, int]]]]
 
@@ -9,11 +9,19 @@ PlayerFuncParam = Union[str, Tuple[str], Dict[str, Dict[str, Union[float, int]]]
 class EmptyClass (Player):
     
     
-    def get_action(self, game_type: str, 
-                        adversary_id: str,
-                        available_actions: Tuple[str], 
-                        adversary_available_actions: Tuple[str], 
-                        payoff_matrix: Dict[str, Dict[str, Union[float, int]]]) -> str:
+    def __init__(self) -> None:
+        self.name = ""
+        self.match_history = dict()
+
+
+    def get_action(
+        self, 
+        game_type: str, 
+        payoff_matrix: PayoffMatrixType,
+        adversary_id: str,
+        match_history: Optional[MatchHistoryType],
+        row_or_col: str
+    ) -> int:
                         
         return ""
 
@@ -28,9 +36,9 @@ def empty_get_action_params() -> Dict[str, PlayerFuncParam]:
     return {
         "game_type": "",
         "adversary_id": "",
-        "available_actions": tuple(),
-        "adversary_available_actions": tuple(),
-        "payoff_matrix": dict()
+        "payoff_matrix": dict(),
+        "match_history": None,
+        "row_or_col": ""
     }
 
 
@@ -43,5 +51,5 @@ class TestEmpty:
     ):
         
         assert empty_class.name == ""
-        assert empty_class.memory == dict()
+        assert empty_class.match_history == dict()
         assert empty_class.get_action(**empty_get_action_params) == ""
